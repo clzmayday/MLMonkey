d791c803148807729c58cb4dd4c4dc6cd00771b0
clzmayday <clzmayday@hotmail.com>
2021-10-14 20:34:40 +0100

    Update

--- test.py
+++ test.py
@@ -33,8 +33,8 @@ WindtexModel.born(data)
 #     csv_w.writerows(csv_data)
 #     file.close()
 
-from sklearn.svm import SVR as model
-trained, trained_ex, valid = WindtexModel.grow(model=model(kernel="poly", degree=5, epsilon=0.1))
+from sklearn.tree import DecisionTreeRegressor as model
+trained, trained_ex, valid = WindtexModel.grow(model=model(criterion="mse", min_samples_split=2, min_samples_leaf=1))
 for i in ["self", "LOO", "RV"]:
     all_result = [[valid[i]["true"][j], valid[i]["predict"][j]] for j in range(len(valid[i]["predict"]))]
     all_result = list(sorted(all_result, key=lambda x: x[0]))
